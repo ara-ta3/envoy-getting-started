08410d77aa990a83665230c8d37b2ea435709b2f
Arata Tanaka <tarata43@yahoo.co.jp>
2015-12-21 00:12:06 +0900

    Modify template

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -2,6 +2,36 @@
 
 @servers (['vm' => 'vagrant@192.168.33.10'])
 
-@task ('ls', ['on' => 'vm'])
-    ls -la
+@setup
+    $start = date('Y/m/d H:i:s');
+@endsetup
+
+@task ('ls', ['on' => 'vm1'])
+    ls -la /tmp
 @endtask
+
+@task ('touch', ['on' => 'vm'])
+    touch /tmp/hoge
+@endtask
+
+@task ('fail', ['on' => 'vm'])
+    test -f /tmp/fuga
+    echo "failed"
+@endtask
+
+@macro ('all')
+    touch
+    ls
+@endmacro
+
+@macro ('all-fail')
+    touch
+    fail
+    ls
+@endmacro
+
+@after
+    echo "after" . PHP_EOL;
+    echo "started: " . $start . PHP_EOL;
+    echo "endted: " . date('Y/m/d H:i:s');
+@endafter
